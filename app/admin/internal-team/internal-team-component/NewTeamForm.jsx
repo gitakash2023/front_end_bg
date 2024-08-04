7bea4d5ce79a9e6b8433bf81e481ca710369575c
gitakash2023 <yogibabu32@gmail.com>
2024-08-04 12:00:26 +0530

    ...

--- app/admin/internal-team/internal-team-component/NewTeamForm.jsx
+++ app/admin/internal-team/internal-team-component/NewTeamForm.jsx
@@ -3,8 +3,26 @@ import React, { useState, useEffect } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { FaUser, FaEnvelope, FaMobile, FaBriefcase, FaBuilding } from 'react-icons/fa';
 import { _create, _update, _getAll } from '../../../../utils/apiUtils';
-import { Snackbar, IconButton, MenuItem, Select, InputLabel, FormControl, Autocomplete, TextField } from '@mui/material';
+import { Snackbar, IconButton, Autocomplete, TextField } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
+
+const roles = [
+    'GenInfo',
+    'EducationInfo',
+    'AddressInfo',
+    'CibilInfo',
+    'ReferenceInfo',
+    'ExperienceInfo'
+];
+
+const inputFields = [
+    { name: 'name', placeholder: 'Name of Team', label: 'Name', type: 'text', icon: <FaUser /> },
+    { name: 'email_id', placeholder: 'Email ID', label: 'Email', type: 'email', icon: <FaEnvelope /> },
+    { name: 'mobile_number', placeholder: 'Phone Number', label: 'Phone Number', type: 'text', icon: <FaMobile /> },
+    { name: 'company_name', placeholder: 'Select Company', label: 'Company Name', type: 'text', icon: <FaBuilding /> },
+    { name: 'role', placeholder: 'Select Role', label: 'Role', type: 'text', icon: <FaBriefcase /> },
+
+];
 
 const roleMapping = {
     'GenInfo': 4,
@@ -15,21 +33,13 @@ const roleMapping = {
     'ExperienceInfo': 9
 };
 
-const inputFields = [
-    { name: 'name', placeholder: 'Name of Team', label: 'Name', type: 'text', icon: <FaUser /> },
-    { name: 'email_id', placeholder: 'Email ID', label: 'Email', type: 'email', icon: <FaEnvelope /> },
-    { name: 'mobile_number', placeholder: 'Phone Number', label: 'Phone Number', type: 'text', icon: <FaMobile /> },   
-     { name: 'company_name', placeholder: 'Select Company', label: 'Company Name', type: 'text', icon: <FaBuilding /> },
-
-    { name: 'role', placeholder: 'Select Role', label: 'Role', type: 'text', icon: <FaBriefcase /> },
-];
-
 const NewTeamForm = ({ team, onClose, updateTeamList }) => {
     const [successMessage, setSuccessMessage] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
     const [snackbarOpen, setSnackbarOpen] = useState(false);
     const [role, setRole] = useState(team ? team.role : '');
     const [companyOptions, setCompanyOptions] = useState([]);
+    const [roleOptions, setRoleOptions] = useState(roles.map(role => ({ label: role })));
     const [selectedCompany, setSelectedCompany] = useState(null);
 
     const initialValues = {
@@ -117,7 +127,7 @@ const NewTeamForm = ({ team, onClose, updateTeamList }) => {
                             validate={(values) => {
                                 const errors = {};
                                 inputFields.forEach(field => {
-                                    if (field.name !== 'role' && !values[field.name]) {
+                                    if (field.name !== 'role' && field.name !== 'company_name' && !values[field.name]) {
                                         errors[field.name] = `${field.label} is required`;
                                     } else if (field.name === 'email_id' && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email_id)) {
                                         errors.email_id = 'Invalid email address';
@@ -134,25 +144,25 @@ const NewTeamForm = ({ team, onClose, updateTeamList }) => {
                                         <div key={index} className="mb-3">
                                             <div className="input-group">
                                                 {field.name === 'role' ? (
-                                                    <FormControl fullWidth>
-                                                        <InputLabel id="role-label">{field.label}</InputLabel>
-                                                        <Field
-                                                            as={Select}
-                                                            name={field.name}
-                                                            labelId="role-label"
-                                                            className="form-control"
-                                                            style={{ backgroundColor: '#f3f4f6', color: 'black', fontSize: '1rem', border: '1px solid #ced4da', borderRadius: '0.25rem' }}
-                                                            onChange={(e) => {
-                                                                const selectedRole = e.target.value;
-                                                                setRole(selectedRole);
-                                                                setFieldValue(field.name, selectedRole);
-                                                            }}
-                                                        >
-                                                            {Object.keys(roleMapping).map((role, idx) => (
-                                                                <MenuItem key={idx} value={role}>{role}</MenuItem>
-                                                            ))}
-                                                        </Field>
-                                                    </FormControl>
+                                                    <Autocomplete
+                                                        value={roleOptions.find(option => option.label === role) || null}
+                                                        onChange={(event, newValue) => {
+                                                            const selectedRole = newValue ? newValue.label : '';
+                                                            setRole(selectedRole);
+                                                            setFieldValue(field.name, selectedRole);
+                                                        }}
+                                                        options={roleOptions}
+                                                        getOptionLabel={(option) => option.label}
+                                                        renderInput={(params) => (
+                                                            <TextField
+                                                                {...params}
+                                                                label={field.label}
+                                                                variant="outlined"
+                                                                placeholder={field.placeholder}
+                                                            />
+                                                        )}
+                                                        className="form-control"
+                                                    />
                                                 ) : field.name === 'company_name' ? (
                                                     <Autocomplete
                                                         value={selectedCompany}
